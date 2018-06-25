5e73bea26b341964c283abe0670c5b7811c28f15
inh556 <yanbin.jin@gmail.com>
2018-06-24 21:26:46 -0700

    init NewsPanel

---
+++ web_server/client/src/NewsPanel/NewsPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class NewsPanel extends React.Component {
+  constructor() {
+    Super();
+    this.state = {
+      news: null
+    };
+  }
+  render() {
+    if(this.state.news) {
+      return(
+        {}
+      )
+    } else {
+      return(
+        {}
+      )
+    }
+  }
+}
