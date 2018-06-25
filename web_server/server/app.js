62826cc371c49e666e004505351385ee835f93ee
inh556 <yanbin.jin@gmail.com>
2018-06-25 13:02:08 -0700

    add CORS

--- web_server/server/app.js
+++ web_server/server/app.js
@@ -10,9 +10,17 @@ app.set('views', path.join(__dirname, '../client/build'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 
+// allow cross 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(logger('dev'));
 app.use('/', index);
 app.use('/news', news);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.status(404);
