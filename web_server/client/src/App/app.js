8060df3ed51fd189860e2e71e2689fdf74dacdf0
inh556 <yanbin.jin@gmail.com>
2018-06-24 21:54:38 -0700

    add NewsPanel to container

--- web_server/client/src/App/app.js
+++ web_server/client/src/App/app.js
@@ -1,6 +1,7 @@
 import  React from 'react';
 import logo from './logo.png';
 import './App.css';
+import NewsPanel from '../NewsPanel/NewsPanel';
 
 class App extends React.Component{
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component{
       <div>
       <img className='logo' src={logo} alt='logo' />
       <div className='container'>
+        <NewsPanel />
       </div>
       </div>
     )
