d8163c00e6faeef9f6c305e0e196fd3f4e8c796d
inh556 <yanbin.jin@gmail.com>
2018-06-24 19:14:11 -0700

    add logo, and modified component name

--- web_server/client/src/App/app.js
+++ web_server/client/src/App/app.js
@@ -1,12 +1,14 @@
 import  React from 'react';
 import logo from './logo.png';
+import './App.css';
 
 class App extends React.Component{
   render() {
     return(
       <div>
       <img className='logo' src={logo} alt='logo' />
-      <div className='container'></div>
+      <div className='container'>
+      </div>
       </div>
     )
   }
