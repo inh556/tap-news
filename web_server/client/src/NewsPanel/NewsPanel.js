5b6fdc8faf4608f8b68ccc5fdedb9828bd725ebd
inh556 <yanbin.jin@gmail.com>
2018-06-24 21:51:14 -0700

    create NewsPanel component

--- web_server/client/src/NewsPanel/NewsPanel.js
+++ web_server/client/src/NewsPanel/NewsPanel.js
@@ -2,20 +2,29 @@ import React from 'react';
 
 class NewsPanel extends React.Component {
   constructor() {
-    Super();
+    super();
     this.state = {
       news: null
     };
   }
+  renderNews(){
+    
+  }
   render() {
     if(this.state.news) {
       return(
-        {}
-      )
+        <div>
+          {this.renderNews()}
+        </div>
+      );
     } else {
       return(
-        {}
-      )
+        <div id="msg-app-loading"> 
+          Loading...
+        </div>
+      );
     }
   }
 }
+
+export default NewsPanel;
