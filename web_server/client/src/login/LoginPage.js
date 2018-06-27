d66206a80dbe722ceaf6cbd9fc45318729aea449
inh556 <yanbin.jin@gmail.com>
2018-06-27 13:14:19 -0700

    clean up

--- web_server/client/src/login/LoginPage.js
+++ web_server/client/src/login/LoginPage.js
@@ -1,47 +1,37 @@
-import LoginForm from './LoginForm';
 import React from 'react';
-
+import LoginForm from './LoginForm';
 class LoginPage extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      errors: {},
+      errors:{},
       user: {
         email: '',
         password: ''
       }
-    };
-  }
-
+    }
+  };
   processForm(event) {
     event.preventDefault();
-
     const email = this.state.user.email;
     const password = this.state.user.password;
-
-    console.log('email:', email);
-    console.log('password', password);
-
-    // TODO: post login data.
+    // need to pass to server later
+    console.log(email);
+    console.log(password);
   }
-
-  changeUser(event) {
+  changeUser(event){
     const field = event.target.name;
     const user = this.state.user;
     user[field] = event.target.value;
-
     this.setState({user});
   }
-
   render() {
     return (
-      <LoginForm
+      <LoginForm 
         onSubmit={(e) => this.processForm(e)}
         onChange={(e) => this.changeUser(e)}
         errors={this.state.errors} />
     )
   }
 }
-
 export default LoginPage;
