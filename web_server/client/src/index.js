12ee06b30b462101130b37016f392e5e58eff917
inh556 <yanbin.jin@gmail.com>
2018-06-27 13:12:32 -0700

    imported new components to test

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import LoginPage from './Login/LoginPage';
+import SignUpPage from './SignUp/SignUpPage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
 registerServiceWorker();
