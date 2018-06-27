7de0b14b488ff6fb64e58bc8659b2999d3ffd089
inh556 <yanbin.jin@gmail.com>
2018-06-27 13:12:11 -0700

    imported materiliza

--- web_server/client/src/App/app.js
+++ web_server/client/src/App/app.js
@@ -1,4 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
 import  React from 'react';
 import logo from './logo.png';
 import './App.css';
