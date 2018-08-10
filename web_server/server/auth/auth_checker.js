72d753c27a44530242442d48e8c7b5891b985550
inh556 <yanbin.jin@gmail.com>
2018-08-10 11:33:22 -0700

    test psuh

--- web_server/server/auth/auth_checker.js
+++ web_server/server/auth/auth_checker.js
@@ -4,10 +4,10 @@ const config = require('../config/config.json');
 
 module.exports = (req, res, next) => {
   console.log('auth_checker: req: ' + req.headers);
-  if(!req.headers.authrization) {
+  if(!req.headers.authorization) {
     return res.status(401).end();
   }
-  const token = req.headers.authrization.split(' ')[1];
+  const token = req.headers.authorization.split(' ')[1];
   console.log('auth_checker: token: ' + token);
   return jwt.verify(token, config.jwtSecret, (err, decoded) => {
     if(err) {return req.status(401).end()};
