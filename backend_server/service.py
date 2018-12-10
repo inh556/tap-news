d317e2d7aa2a558e1023e1eaebe956007525b16b
inh556 <yanbin.jin@gmail.com>
2018-12-10 14:24:33 -0800

    added click event

--- backend_server/service.py
+++ backend_server/service.py
@@ -20,9 +20,18 @@ def get_news_summaries_for_user(user_id, page_num):
   LOGGER.debug("get_news_summaries_for_user is called with %s and %s", user_id, page_num)
   return operations.getNewsSummariesForUser(user_id, page_num)
 
+def log_news_click_for_user(user_id, news_id):
+  # Log a news click event for a user.
+  LOGGER.debug("log_news_click_for_user is called with %s and %s", user_id, news_id)
+  return operations.logNewsClickForUser(user_id, news_id)
 
-server = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
-server.register_function(add, 'add')
+  
+# threading RPC Server
+RPC_SERVER = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
+RPC_SERVER.register_function(add, 'add')
+RPC_SERVER.register_function(get_one_news, 'getOneNews')
+RPC_SERVER.register_function(get_news_summaries_for_user, 'getNewsSummariesForUser')
+RPC_SERVER.register_function(log_news_click_for_user, 'logNewsClickForUser')
 logger.info('starting RPC server on %s: %d' SERVER_HOST, SERVER_PORT)
 
 server.serve_forever()
